e354e6969f8babf16303c5488ba58d6464a3dfd8
SaurabhTiwari95 <saurabhtiwari1707@gmail.com>
2020-03-25 04:30:11 +0530

    TEST 25 march

--- services/csv-generator.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { AngularCsv } from "angular7-csv/dist/Angular-csv";
-
-@Injectable()
-export class CsvGeneratorService {
-  constructor() {}
-
-  downloadCsv(data, fileName, options?) {}
-}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component:HomeComponent },
-  { path: 'lazy', loadChildren: './lazy/lazy.modu;e#LazyModule' }
+  { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule' }
 ];
 
 @NgModule({
